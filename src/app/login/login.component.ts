a41f144141393a12f7d34b6461b3d0d38f3860cc
JianhangLin <linjianhang1010@gmail.com>
2020-06-16 11:08:33 +0800

    feat(login): use text show info

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  text = 'Hello LoginComponent';
   constructor() { }
 
   ngOnInit(): void {
