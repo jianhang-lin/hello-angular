aa87242cae56923e832e690f1f8444767f78c71f
JianhangLin <linjianhang1010@gmail.com>
2020-06-17 09:35:00 +0800

    feat(todo): add addTodo method

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,22 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import {Todo} from './todo.model';
+import { TodoService } from './todo.service';
+import { Todo } from './todo.model';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
+  providers: [TodoService]
 })
 export class TodoComponent implements OnInit {
 
   todos: Todo[] = [];
   desc = '';
-  constructor() { }
+  constructor(private service: TodoService) { }
 
   ngOnInit(): void {
   }
 
   addTodo() {
-    this.todos.push({id: 1, desc: this.desc, completed: false});
+    this.todos = this.service.addTodo(this.desc);
     this.desc = '';
   }
 }
--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { v4 } from 'uuid';
+import { Todo } from './todo.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
+  todos: Todo[] = [];
   constructor() { }
+
+  addTodo(todoItem: string): Todo[] {
+    const todo = {
+      id: v4(),
+      desc: todoItem,
+      completed: false
+    };
+    this.todos.push(todo);
+    return this.todos;
+  }
 }
