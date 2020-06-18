c9663fe4f0dbeb65bd82aecbde1abf1674809877
JianhangLin <linjianhang1010@gmail.com>
2020-06-18 14:58:55 +0800

    fix(todo): move code to todo footer component

--- src/app/todo/todo-footer/todo-footer.component.ts
+++ src/app/todo/todo-footer/todo-footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-todo-footer',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoFooterComponent implements OnInit {
 
+  @Input() itemCount: number;
   constructor() { }
 
   ngOnInit(): void {
