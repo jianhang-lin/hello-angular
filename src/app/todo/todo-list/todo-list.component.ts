6607feb1d80151f86956661c67d4ed488d2a5517
JianhangLin <linjianhang1010@gmail.com>
2020-06-19 14:18:49 +0800

    feat(todo): make todo list component

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Todo} from '../todo.model';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
+  todosTemp: Todo[] = [];
+  @Input()
+  set todos(todos: Todo[]) {
+    this.todosTemp = [...todos];
+  }
+
+  get todos() {
+    return this.todosTemp;
+  }
+
+  @Output() doRemoveTodo = new EventEmitter<Todo>();
+  @Output() doToggleTodo = new EventEmitter<Todo>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  doRemoveTriggered(todo: Todo) {
+    this.doRemoveTodo.emit(todo);
+  }
+
+  doToggleTriggered(todo: Todo) {
+    this.doToggleTodo.emit(todo);
   }
 
 }
