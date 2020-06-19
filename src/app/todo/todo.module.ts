78348fa1ca4128f3db4f1cf0aef32068f433b2ab
JianhangLin <linjianhang1010@gmail.com>
2020-06-19 09:49:34 +0800

    fix(todo): add todo module define

---
+++ src/app/todo/todo.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { routing } from './todo.routes';
+import { TodoComponent } from './todo.component';
+import { TodoFooterComponent } from './todo-footer/todo-footer.component';
+import { TodoHeaderComponent } from './todo-header/todo-header.component';
+import { TodoService } from './todo.service';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    routing
+  ],
+  declarations: [
+    TodoComponent,
+    TodoFooterComponent,
+    TodoHeaderComponent
+  ],
+  providers: [
+    {provide: 'todoService', useClass: TodoService}
+  ]
+})
+export class TodoModule {
+}
