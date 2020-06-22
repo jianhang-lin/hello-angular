e4243b6c18a2ae3a5e9158e5d89b5c1ee5a6c2d8
JianhangLin <linjianhang1010@gmail.com>
2020-06-22 14:02:41 +0800

    feat(core): implement auth-guard service

--- src/app/core/auth-guard.service.ts
+++ src/app/core/auth-guard.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    // 取得用记访问的URL
+    const url: string = state.url;
+    return this.checkLogin(url);
+  }
+
+
+  private checkLogin(url: string): boolean {
+    // 如果用户已经登录就发行
+    if (localStorage.getItem('userId') != null) {
+      return true;
+    }
+    // 否则，存储要访问的URL到本地
+    localStorage.setItem('redirectUrl', url);
+    // 然后导航到登录页面
+    this.router.navigate(['/login']);
+    // 返回false, 取消导航
+    return false;
+  }
 }
--- src/app/todo/todo.routes.ts
+++ src/app/todo/todo.routes.ts
@@ -1,7 +1,8 @@
 import { RouterModule, Routes } from '@angular/router';
 import { TodoComponent } from './todo.component';
+import { AuthGuardService } from '../core/auth-guard.service';
 
 export const routes: Routes = [
-  {path: 'todo/:filter', component: TodoComponent}
+  {path: 'todo/:filter', canActivate: [AuthGuardService], component: TodoComponent}
 ];
 export const routing = RouterModule.forChild(routes);
