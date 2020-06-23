6548bc2e081c1a66269f11a987dff8977ea9c1bb
JianhangLin <linjianhang1010@gmail.com>
2020-06-23 12:46:42 +0800

    feat(playground): add playground component

--- src/app/playground/playground.module.ts
+++ src/app/playground/playground.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PlaygroundComponent } from './playground.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PlaygroundComponent],
   imports: [
     CommonModule
   ]
