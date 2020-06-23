f58068621b221591329271b28d5ac915ec809526
JianhangLin <linjianhang1010@gmail.com>
2020-06-23 14:50:28 +0800

    feat(palyground): add one component

--- src/app/playground/playground.module.ts
+++ src/app/playground/playground.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlaygroundRoutingModule } from './playground-routing.module';
 import { PlaygroundComponent } from './playground.component';
+import { OneComponent } from './one/one.component';
 
 @NgModule({
-  declarations: [PlaygroundComponent],
+  declarations: [PlaygroundComponent, OneComponent],
   imports: [
     CommonModule,
     PlaygroundRoutingModule
