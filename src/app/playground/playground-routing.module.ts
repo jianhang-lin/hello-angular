5e75565f6ce47492325dff6d5fdd343a0a4ac268
JianhangLin <linjianhang1010@gmail.com>
2020-06-23 14:54:52 +0800

    feat(palyground): add one and two component

--- src/app/playground/playground-routing.module.ts
+++ src/app/playground/playground-routing.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PlaygroundComponent } from './playground.component';
+import {OneComponent} from './one/one.component';
+import {TwoComponent} from './two/two.component';
 
 export const routes: Routes = [
-  {path: '', component: PlaygroundComponent}
+  {
+    path: '',
+    component: PlaygroundComponent,
+    children: [
+      {
+        path: 'one',
+        component: OneComponent
+      },
+      {
+        path: 'two',
+        component: TwoComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
