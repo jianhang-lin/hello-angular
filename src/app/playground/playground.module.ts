24ca4033c0a4bc7324cc36f6285c363f77f5e6a0
JianhangLin <linjianhang1010@gmail.com>
2020-06-24 15:38:22 +0800

    feat(palyground): use diy pipe

--- src/app/playground/playground.module.ts
+++ src/app/playground/playground.module.ts
@@ -5,9 +5,10 @@ import { PlaygroundComponent } from './playground.component';
 import { OneComponent } from './one/one.component';
 import { TwoComponent } from './two/two.component';
 import { ThreeComponent } from './three/three.component';
+import { TrimSpacePipe } from './trim-space.pipe';
 
 @NgModule({
-  declarations: [PlaygroundComponent, OneComponent, TwoComponent, ThreeComponent],
+  declarations: [PlaygroundComponent, OneComponent, TwoComponent, ThreeComponent, TrimSpacePipe],
   imports: [
     CommonModule,
     PlaygroundRoutingModule
