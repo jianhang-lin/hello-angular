749640beaf852446205ead435716841517b797af
JianhangLin <linjianhang1010@gmail.com>
2020-06-24 15:21:30 +0800

    feat(app): remove unused path define

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent},
   {path: 'todo', redirectTo: 'todo/ALL'},
   {path: 'playground', loadChildren: () => import('./playground/playground.module').then(mod => mod.PlaygroundModule)},
 ];
