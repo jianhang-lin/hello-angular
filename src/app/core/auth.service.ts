75fdbc27772fcea7eb9a00be987012e9fe94c3f4
JianhangLin <linjianhang1010@gmail.com>
2020-06-24 14:44:49 +0800

    feat(auth): loginWithCredentials return Observable

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,6 +1,8 @@
 import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Auth } from '../domain/entities';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Auth, User } from '../domain/entities';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +11,9 @@ export class AuthService {
 
   constructor(private http: HttpClient, @Inject('user') private userSerivce) { }
 
-  loginWithCredentials(username: string, password: string): Promise<Auth> {
+  loginWithCredentials(username: string, password: string): Observable<Auth> {
     // return username === 'Grant';
-    return this.userSerivce.findUser(username).toPromise().then(user => {
+    return this.userSerivce.findUser(username).pipe(map((user: User) => {
       const auth = new Auth();
       localStorage.removeItem('userId');
       const redirectUrl = (localStorage.getItem('redirectUrl') == null) ? '/' : localStorage.getItem('redirectUrl');
@@ -22,17 +24,12 @@ export class AuthService {
       } else if (password === user.password) {
         auth.user = Object.assign({}, user);
         auth.hasError = false;
-        localStorage.setItem('userId', user.id);
+        localStorage.setItem('userId', String(user.id));
       } else {
         auth.hasError = true;
         auth.errMsg = 'password not match';
       }
       return auth;
-    }).catch(this.handleError);
-  }
-
-  private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error);
-    return Promise.reject(error.message || error);
+    }));
   }
 }
