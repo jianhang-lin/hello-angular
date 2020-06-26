486ff8acc6c17e384920497b38871c2d1de296ba
JianhangLin <linjianhang1010@gmail.com>
2020-06-26 16:03:45 +0800

    feat(playground): use keyframes

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -19,12 +19,12 @@ import { animate, keyframes, state, style, transition, trigger } from '@angular/
         height: '50px'
       })),
       transition('void => *', animate(5000, keyframes([
-        style({transform: 'scale(0)'}),
-        style({transform: 'scale(0.1)'}),
-        style({transform: 'scale(0.5)'}),
-        style({transform: 'scale(0.9)'}),
-        style({transform: 'scale(0.95)'}),
-        style({transform: 'scale(1)'})
+        style({transform: 'scale(0)', padding: '0px'}),
+        style({transform: 'scale(0.1)', padding: '50px'}),
+        style({transform: 'scale(0.5)', padding: '100px'}),
+        style({transform: 'scale(0.9)', padding: '120px'}),
+        style({transform: 'scale(0.95)', padding: '135px'}),
+        style({transform: 'scale(1)', padding: '140px'})
       ]))),
       transition('* => *', animate('0.5s 1s cubic-bezier(0.175, 0.885, 0.32, 1.275)'))
     ])
