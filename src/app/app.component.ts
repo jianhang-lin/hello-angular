310afbe92b09effb6802e2653e82837111c7b57b
JianhangLin <linjianhang1010@gmail.com>
2020-06-26 14:32:41 +0800

    feat(app): subscribe auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Auth } from './domain/entities';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  auth: Auth;
   title = 'This is a hello-angular app';
+
+  constructor(@Inject('auth') private service, private router: Router) {
+  }
+
+  ngOnInit() {
+    this.service.getAuth().subscribe(auth => this.auth = Object.assign({}, auth));
+  }
+
+  login() {
+    this.router.navigate(['login']);
+  }
+
+  logout() {
+    this.service.unAuth();
+    this.auth = null;
+    this.router.navigate(['login']);
+  }
+
 }
