5c97731ee2c43515777f43e1ab170e591f508cd9
JianhangLin <linjianhang1010@gmail.com>
2020-06-26 14:30:42 +0800

    feat(login): inject bing service

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { BingImageService } from './bing-image.service';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,11 @@ import { LoginComponent } from './login.component';
   imports: [
     SharedModule,
     LoginRoutingModule
+  ],
+  providers: [
+    {
+      provide: 'bing', useClass: BingImageService
+    }
   ]
 })
 export class LoginModule { }
