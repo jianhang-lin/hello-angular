08b7c9d5e50a7c08237b5a3aa20a5a5b9720e69b
JianhangLin <linjianhang1010@gmail.com>
2020-06-26 15:43:56 +0800

    feat(playground): add animations

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -39,7 +39,7 @@ export class LoginComponent implements OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.subscription?.unsubscribe();
   }
 
   onSubmit() {
--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -1,17 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-playground',
   templateUrl: './playground.component.html',
-  styleUrls: ['./playground.component.scss']
+  styleUrls: ['./playground.component.scss'],
+  animations: [
+    trigger('signal', [
+      state('void', style({
+        transform: 'translateY(-100%)'
+      })),
+      state('go', style({
+        'background-color': 'green',
+        height: '100px'
+      })),
+      state('stop', style({
+        'background-color': 'red',
+        height: '50px'
+      })),
+      transition('void => *', animate(500))
+    ])
+  ]
 })
 export class PlaygroundComponent implements OnInit {
 
   birthday = new Date();
   pi = 3.141592627;
+  signal: string;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onGo() {
+    this.signal = 'go';
+  }
+
+  onStop() {
+    this.signal = 'stop';
+  }
 }
