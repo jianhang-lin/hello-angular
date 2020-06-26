3947db114ef2082572059eaa35381c1fac6c0dce
JianhangLin <linjianhang1010@gmail.com>
2020-06-26 16:14:29 +0800

    feat(login): add register dialog component

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,10 +3,12 @@ import { SharedModule } from '../shared/shared.module';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { BingImageService } from './bing-image.service';
+import { RegisterDialogComponent } from './register-dialog/register-dialog.component';
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    RegisterDialogComponent
   ],
   imports: [
     SharedModule,
